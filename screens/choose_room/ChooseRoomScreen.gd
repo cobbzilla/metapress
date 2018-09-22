78e11de22ecdde751825abc1b4e54993a9125888
Jonathan Cobb <jonathan@kyuss.org>
2018-09-22 14:01:31 -0400

    refactor API framework, use singleton

--- screens/choose_room/ChooseRoomScreen.gd
+++ screens/choose_room/ChooseRoomScreen.gd
@@ -5,5 +5,4 @@ func _ready():
 
 func handle_api_response (room):
 	print("joined room: {room}".format({ "room": room }))
-	$ApiSession.room = room
 	get_tree().change_scene("res://screens/game_board/GameBoardScreen.tscn")
