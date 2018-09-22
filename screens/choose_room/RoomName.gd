5706b66d3f9e89e57a1fc41148a8bdf33e3eddaa
Jonathan Cobb <jonathan@kyuss.org>
2018-09-22 14:07:47 -0400

    rooms listing now working

--- screens/choose_room/RoomName.gd
+++ screens/choose_room/RoomName.gd
@@ -1,8 +1,10 @@
-extends MenuButton
+extends OptionButton
 
 func _ready():
 	$ListRoomsApi.api_init(self)
 	$ListRoomsApi.call_deferred("list_rooms")
 
 func handle_api_response (rooms):
-	print("found rooms: {rooms}".format({ "rooms": rooms }))
+	for room in rooms:
+		add_item(room.name)
+		print("found room: {room}".format({ "room": room.name }))
