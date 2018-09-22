4aacb71466d86f4fdb5a1c7ffa38354f8785b660
Jonathan Cobb <jonathan@kyuss.org>
2018-09-22 14:18:33 -0400

    joining game now works

--- screens/choose_room/ChooseRoomScreen.gd
+++ screens/choose_room/ChooseRoomScreen.gd
@@ -6,3 +6,7 @@ func _ready():
 func handle_api_response (room):
 	print("joined room: {room}".format({ "room": room }))
 	get_tree().change_scene("res://screens/game_board/GameBoardScreen.tscn")
+
+func _on_JoinRoomButton_pressed():
+	var roomName = $VBoxContainer/RoomName
+	$JoinRoomApi.join(roomName.get_item_text(roomName.selected))
